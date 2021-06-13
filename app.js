2da0498958c2fd7a777ecca77e9f9cf406679b68
PathakAadarsh <73171424+PathakAadarsh@users.noreply.github.com>
2021-06-13 17:19:04 +0530

    Refactor
    Comments removed.
    Changed the title.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", init);
 function init() {
   
     document.getElementById("btnSearch").addEventListener("click", ev => {
-    ev.preventDefault(); //to stop the page reload
+    ev.preventDefault(); 
     let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=1&q=`;
     let str = document.getElementById("search").value.trim();
     url = url.concat(str);
@@ -12,20 +12,19 @@ function init() {
     fetch(url)
       .then(response => response.json())
       .then(content => {
-        //  data, pagination, meta
+      
         console.log(content.data);
         console.log("META", content.meta);
         let fig = document.createElement("figure");
         let img = document.createElement("img");
-        //let fc = document.createElement("figcaption");
+        
         img.src = content.data[0].images.downsized.url;
-      //  img.alt = content.data[0].title;
-        //fc.textContent = content.data[0].title;
+      
         fig.appendChild(img);
-        //fig.appendChild(fc);
+      
         let out = document.querySelector(".out");
         out.insertAdjacentElement("afterbegin", fig);
-        document.querySelector("#search").value = "";  //make the search box empty again
+        document.querySelector("#search").value = "";  
       })
       .catch(err => {
         console.error(err);
